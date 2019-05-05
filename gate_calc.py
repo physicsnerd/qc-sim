2f17c79cd73e80e53010bd3dbce0bb97696553b5
physicsnerd <heather.nerd@gmail.com>
2019-05-05 14:28:56 -0700

    calculator to scale gates appropriately...maybe add way to save gates, so qc-sim can take them in as input

---
+++ gate_calc.py
@@ -0,0 +1,35 @@
+import numpy as np
+import math
+import cmath
+
+qnum = int(input('number of qubits: '))
+
+def gate_scale(gate, ap_qubit):
+    dimensions = int(math.sqrt(np.size(gate)))
+    ap_qubit-=1
+    if 2**qnum == dimensions:
+        return gate
+    else:
+        iterator = 1
+        kron_num = []
+        identity = np.identity(dimensions, np.matrix)
+        while iterator <= dimensions:
+            kron_num.append(identity)
+            iterator+=1
+        kron_num[ap_qubit] = gate
+        kron_iterator = list(range(len(kron_num)))
+        for i in kron_iterator:
+            if i == 0:
+                x = kron_num[i]
+            if i > 0:
+                x = np.kron(x, kron_num[i])
+        return x
+
+dimension = int(input('size of matrix: '))
+value_hold = []
+for y in range(dimension):
+    for x in range(dimension):
+        value_hold.append(eval(input('What value for position ({}, {}): '.format(y+1, x+1))))
+matrix = np.matrix(np.resize(value_hold, (dimension, dimension)))
+ap_qubit = int(input('qubit to apply to: '))
+print(gate_scale(matrix, ap_qubit))
